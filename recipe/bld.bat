a8366db2e28317a6f19bca35d77635dfa680ad0a
Mervin Fansler <mervin.fansler@bric.ku.dk>
2026-06-16 09:38:24 +0200

    update regex

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
-sed -i 's/^[[:space:]]*ar rvs/[[:space:]]*$(AR) rvs/' src/leidenalg/Makefile.win
-sed -i 's/^[[:space:]]*ar rvs/[[:space:]]*$(AR) rvs/' src/rigraph/Makefile.win
+sed -i "s/^\([[:space:]]*\)ar rvs/\1\$(AR) rvs/" src/leidenalg/Makefile.win
+sed -i "s/^\([[:space:]]*\)ar rvs/\1\$(AR) rvs/" src/rigraph/Makefile.win
 
 "%R%" CMD INSTALL --build . %R_ARGS%
 IF %ERRORLEVEL% NEQ 0 exit /B 1
